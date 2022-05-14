5fad145db33d61a8f030617539fee2e7172c6384
bcmeter <87074315+bcmeter@users.noreply.github.com>
2022-05-14 23:14:38 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ fi
 if [ "$1" != "update" ]; then
 
 
-echo "\e[34mInstalling software packages needed to run bcMeter. This will take a while and is dependent on your internet connection, the amount of updates and the speed of your pi."
+echo "Installing software packages needed to run bcMeter. This will take a while and is dependent on your internet connection, the amount of updates and the speed of your pi."
 apt update && apt upgrade -y && apt install -y i2c-tools zram-tools python3-pip python3-smbus python3-dev python3-rpi.gpio python3-numpy nginx php php-fpm php-pear php-common php-cli php-gd screen git && pip3 install gpiozero adafruit-blinka tabulate && systemctl enable zramswap.service  
 git clone https://github.com/bcmeter/bcmeter.git /home/pi/bcmeter
   
@@ -29,17 +29,17 @@ if [ "$1" != "update" ]; then
 mkdir /home/pi/logs
 touch /home/pi/logs/log_current.csv
 
-echo "\e[34mInstalling common Temperature sensors (DHT22/DHT11 and BMP180/280)"
+echo "Installing common Temperature sensors (DHT22/DHT11 and BMP180/280)"
 git clone https://github.com/coding-world/Python_BMP.git && cd Python_BMP/ &&  python3 setup.py install 
 pip3 install Adafruit_Python_DHT
 fi
-echo "\e[34mConfiguring"
+echo "Configuring"
 raspi-config nonint do_onewire 0
 sh -c "echo 'dtoverlay=w1-gpio,gpiopin=5' >> /boot/config.txt"
 raspi-config nonint do_boot_behaviour B2
-echo "\e[34menabled autologin - you can disable this with sudo raspi-config anytime"
+echo "enabled autologin - you can disable this with sudo raspi-config anytime"
 raspi-config nonint do_i2c 0
-echo "\e[34menabled i2c"
+echo "enabled i2c"
 if [ "$1" != "update" ]; then
 mv /home/pi/nginx-bcMeter.conf /etc/nginx/sites-enabled/default
 
@@ -51,13 +51,13 @@ echo "pi  ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/pi
 
 systemctl start nginx
 
-echo "\e[34menabled webserver."
+echo "enabled webserver."
 echo "\e[104m!if you get a 502 bad gateway error in browser, check PHP-FPM version in /etc/nginx/sites-enabled/default is corresponding to installed php version!"
 
 read -p "which hostname / address should be used for access (user interface in browser, ssh)? (for example: bcmeter01):" hostname  
 raspi-config nonint do_hostname $hostname
 
-echo "\e[34mconfiguration complete. default timezone is UTC+0 - you can change it with 'sudo raspi-config'. "
+echo "configuration complete. default timezone is UTC+0 - you can change it with 'sudo raspi-config'. "
 fi
 
 if [ "$1" != "update" ]; then
@@ -66,21 +66,28 @@ if ! grep -q "bcMeter.py" /home/pi/.bashrc; then
     case $yn in
         [Yy]* ) echo -e "#autostart bcMeter \n if ! pgrep -f "bcMeter.py" > /dev/null \n then sudo screen python3 /home/pi/bcMeter.py \n else python3 /home/pi/output.py \n fi " >> /home/pi/.bashrc; break;;
         [Nn]* ) break;;
-        * ) echo "\e[34mPlease answer yes or no.";;
+        * ) echo "Please answer yes or no.";;
     esac
 fi
 
-read -p "\e[34mDo you wish to start the script NOW? You can always stop it by pressing ctrl+c. " yn
+read -p "Do you wish to start the script NOW? You can always stop it by pressing ctrl+c. " yn
     case $yn in
         [Yy]* ) screen python3 /home/pi/bcMeter.py; break;;
         [Nn]* ) exit;;
-        * ) echo "\e[34mPlease answer yes or no.";;
+        * ) echo "Please answer yes or no.";;
     esac
 fi
 if [ "$1" == "update" ]; then
 screen python3 /home/pi/bcMeter.py
-rm -rf /home/pi/bcmeter
+
 fi
+rm -rf /home/pi/bcmeter
 chmod -R 777 /home/pi/
 
-
+read -p "Do you wish to finish configuration by rebooting? " yn
+    case $yn in
+        [Yy]* ) reboot now; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+fi

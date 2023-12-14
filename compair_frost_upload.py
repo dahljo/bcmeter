a415aaedc977d219c6fd69aea087facd5f0ea5aa
jonas dahl <77916034+dahljo@users.noreply.github.com>
2023-12-14 16:14:31 +0100

    Accuracy improvements
    Big update, approaching 1.0!
    
    Crucial compatibility / accuracy update
    
    This is a major overhaul of everything.
    
    * rework of the interface
    * reworked sample requesting function
    * reworked FROST  implementation
    * Filtering the values
    * updated install routine
    * optional sending logs by mail
    * Now we have a more granular log accessible.
    
    * Fixed bugs and bugs and bugs
    * Location can only be updated if historical data is available
    * Online Check for FROST upload
    
    * Updating Install/Update routine
    * Updated debugging messages

--- compair_frost_upload.py
+++ compair_frost_upload.py
@@ -2,12 +2,61 @@
 import requests
 import json
 import uuid
-import datetime
 import random
 import socket
 import bcMeterConf
-
+import csv
+import os
 from time import sleep
+import logging
+import bcMeterConf
+from datetime import datetime
+
+# Create the log folder if it doesn't exist
+log_folder = '/home/pi/maintenance_logs/'
+log_entity = 'compair_frost_upload'
+os.makedirs(log_folder, exist_ok=True)
+
+# Create a logger
+logger = logging.getLogger(f'{log_entity}_log')
+logger.setLevel(logging.DEBUG)  # Set the logging level to DEBUG
+
+
+# Clear the handlers to avoid duplicate log messages
+logger.handlers.clear()
+
+# Configure the log file with a generic name
+log_file_generic = f'{log_folder}{log_entity}.log'
+if os.path.exists(log_file_generic):
+	os.remove(log_file_generic)
+handler_generic = logging.FileHandler(log_file_generic)
+handler_generic.setLevel(logging.DEBUG)
+formatter_generic = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+handler_generic.setFormatter(formatter_generic)
+
+# Configure the log file with a timestamp in its filename
+current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_file_timestamped = f'{log_folder}{log_entity}_{current_datetime}.log'
+handler_timestamped = logging.FileHandler(log_file_timestamped)
+handler_timestamped.setLevel(logging.DEBUG)
+formatter_timestamped = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+handler_timestamped.setFormatter(formatter_timestamped)
+
+# Add both handlers to the logger
+logger.addHandler(handler_generic)
+logger.addHandler(handler_timestamped)
+
+log_file_prefix = f'{log_entity}_'
+log_files = [f for f in os.listdir(log_folder) if f.startswith(log_file_prefix) and f.endswith('.log')]
+log_files.sort()
+if len(log_files) > 11:
+	files_to_remove = log_files[:len(log_files) - 11]
+	for file_to_remove in files_to_remove:
+		os.remove(os.path.join(log_folder, file_to_remove))
+
+
+
+logger.debug("Compair FROST Upload initialized")
 
 
 # endpoint for checking internet connection (this is Google's public DNS server)
@@ -28,8 +77,95 @@ name_temperature = "Temperature"
 name_coordinates = "Position"
 name_filter_status ="Filter Status"
 
-location = bcMeterConf.location[::-1]
-print(location)
+def check_connection():
+	current_time = 0
+	while current_time < 5:
+		try:
+			socket.setdefaulttimeout(3)
+			socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+			return True
+		except Exception:
+			current_time += 1
+			sleep(1)
+
+def get_location():
+	import json
+	import requests 
+	my_ip = requests.get('https://api.ipify.org').text
+	my_loc = requests.get('https://ipinfo.io/'+my_ip).text
+	my_loc = json.loads(my_loc)
+	my_lat =  float(my_loc['loc'].split(',')[0])
+	my_lon = float(my_loc['loc'].split(',')[1])
+	return [my_lat,my_lon]
+
+location= getattr(bcMeterConf,'location',[0,0])
+
+online = check_connection()
+
+if (online is True) and (bcMeterConf.get_location is True) and (location[0]==0):
+	location = get_location()
+	print("adding location to conf")
+	if not 'location' in open('bcMeterConf.py').read():
+		with open('bcMeterConf.py', 'a') as f:
+			f.write("location=" + str(location) + "#Location of the bcMeter. Keep syntax exactly like that [lat,lon]#session")
+	else:
+		with open('bcMeterConf.py', 'r') as f:
+			lines = f.readlines()
+		for i, line in enumerate(lines):
+			if line.startswith('location'):
+				lines[i] = "location=" + str(location) + "#Location of the bcMeter. Keep syntax exactly like that [lat,lon]#session"
+		with open('bcMeterConf.py', 'w') as f:
+			f.writelines(lines)
+	importlib.reload(bcMeterConf)
+
+location = bcMeterConf.location[::-1] #reverse to comply to frost syntax
+
+if (location[0]==0):
+	logger.error("No valid Location for bcMeter given, cancelling Upload. Enter manually or check options and internet connection")
+	sys.exit(1)
+
+
+def update_location(location):
+	url="https://services.wecompair.eu/receiver/sensors/Locations"
+	old_location_request = requests.get(f"https://sensorthings.wecompair.eu/FROST-Server/v1.1/Things('{thing_id}')?$expand=Locations").text
+	request_json = json.loads(old_location_request)
+	code = request_json.get('code', 0)
+	if (code != 404):
+		longitude = request_json['Locations'][0]['location']['geometry']['coordinates'][1]
+		latitude = request_json['Locations'][0]['location']['geometry']['coordinates'][0]
+
+		old_location = [latitude, longitude]
+	else:
+		old_location = [0,1]
+
+	old_location = [0, 1] if code == 404 else request_json['Locations'][0]['location']['geometry']['coordinates'][::-1]
+
+	if (old_location != location):
+		logger.debug("updating location")
+		new_location_body = {
+				"name": "BcMeter",
+				"description": "BcMeter",
+				"encodingType": "application/geo+json",
+				"location": {
+					"type": "Feature",
+					"properties": {},
+					"geometry": {
+						"type": "Point",
+						"coordinates": location
+					}
+				},
+				"Things": [
+				{ "@iot.id": f"{thing_id}"}
+			  ]
+			}		
+		res = requests.post(url, json=new_location_body) 
+		logger.debug("updated location %s, %s, %s", thing_id, location, res)
+
+	
+
+if (online is True):
+	update_location(location)
+
 
 def get_frost_observations(timestamp, bcngm3,atn,bcmsen,bcmref,bcmtemperature,location, filter_status):
 	data_streams = [
@@ -80,42 +216,25 @@ def get_frost_observations(timestamp, bcngm3,atn,bcmsen,bcmref,bcmtemperature,lo
 
 
 
-def check_connection():
-	connection_ok = False
-	current_time = 0
-	while current_time < 5:
-		try:
-			socket.setdefaulttimeout(DNS_TIME_OUT)
-			socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((DNS_HOST, DNS_PORT))
-			connection_ok = True
-			break
-		except Exception as e:
-			current_time += 1
-			print("Caught exception: ", e)
-			sleep(1)
-	return connection_ok
-
 
 def get_timestamp():
-	dateTimeObj = datetime.datetime.now()
+	dateTimeObj = datetime.now()
 	return dateTimeObj.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-def check_if_sensor_registered(): #to be adjusted for FROST
+def check_if_sensor_registered(): 
 	url = "https://services.dev.wecompair.eu/data/sensors/exists?thingIdentifier=" + thing_id
 	registered = requests.get(url).status_code
 	if (registered == 404):
 		url = "https://sensorthings.wecompair.eu/FROST-Server/v1.1/$batch"
-
-
-		body = {
+		thing_id_body = {
 			"@iot.id": thing_id,
 			"name": thing_id,
 			"description": "bcMeter",
-            "properties": {
-                "source": "bcmeter",
-                "dataType": "AIR"
-            }
-			Locations:[
+			"properties": {
+				"source": "bcmeter",
+				"dataType": "AIR"
+			},
+			"Locations":[
 			{
 				"name": "Citizen Science Device",
 				"description": "Citizen Science Device",
@@ -307,16 +426,12 @@ def check_if_sensor_registered(): #to be adjusted for FROST
 
 
 		res = requests.post(url, json=save_the_thing) 
-		print("This device registered:",res.status_code)
+		logger.debug("Registered this device at CompAIR: %s",res.status_code)
 
 	if (registered == 200):
-		print("bcMeter already registered!")
-
-
-online = check_connection()
+		logger.debug("bcMeter already registered at CompAIR!")
 
 if (online is True):
-	print("checking if sensor is registered")
 	check_if_sensor_registered()
 
 
@@ -324,5 +439,95 @@ def upload_sample(bcngm3,atn,bcmsen,bcmref,bcmtemperature, location, filter_stat
 	timestamp = get_timestamp()
 	observations = get_frost_observations(timestamp, bcngm3,atn,bcmsen,bcmref,bcmtemperature,location, filter_status)
 	url = "https://sensorthings.wecompair.eu/FROST-Server/v1.1/CreateObservations"
-	res = requests.post(url, json=observations) 
-	print(observations)
+	try:
+		res = requests.post(url, json=observations)
+		logger.debug(f"uploaded sample to FROST with return code {res}") 
+	except requests.exceptions.RequestException as e:
+		logger.error(f"An error occurred: {e}")
+
+def compair_frost_upload_offline_log(compair_offline_log_path):
+	data_streams = []
+
+	with open(compair_offline_log_path, 'r') as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=";")
+		for row in reader:
+			if row != "":
+				timestamp = str(row['timestamp'])
+				bcngm3 = float(row['bcngm3'])
+				atn = float(row['atn'])
+				bcmsen = float(row['bcmsen'])
+				bcmref = float(row['bcmref'])
+				bcmtemperature = float(row['bcmtemperature'])
+				filter_status = int(row['filter_status'])
+
+				data_streams.append({
+					"Datastream": {
+						"@iot.id": f"{thing_id}_bc"
+					},
+					"components": ["phenomenonTime", "result"],
+					"dataArray": [[timestamp, bcngm3]],
+				})
+				data_streams.append({
+					"Datastream": {
+						"@iot.id": f"{thing_id}_atn"
+					},
+					"components": ["phenomenonTime", "result"],
+					"dataArray": [[timestamp, atn]],
+				})
+				data_streams.append({
+					"Datastream": {
+						"@iot.id": f"{thing_id}_sen"
+					},
+					"components": ["phenomenonTime", "result"],
+					"dataArray": [[timestamp, bcmsen]],
+				})
+				data_streams.append({
+					"Datastream": {
+						"@iot.id": f"{thing_id}_ref"
+					},
+					"components": ["phenomenonTime", "result"],
+					"dataArray": [[timestamp, bcmref]],
+				})
+				data_streams.append({
+					"Datastream": {
+						"@iot.id": f"{thing_id}_temp"
+					},
+					"components": ["phenomenonTime", "result"],
+					"dataArray": [[timestamp, bcmtemperature]],
+				})
+				data_streams.append({
+					"Datastream": {
+						"@iot.id": f"{thing_id}_filter_status"
+					},
+					"components": ["phenomenonTime", "result"],
+					"dataArray": [[timestamp, filter_status]],
+				})
+
+
+	result_dict = {}
+	for obj in data_streams:
+		if 'Datastream' in obj and isinstance(obj['Datastream'], dict) and '@iot.id' in obj['Datastream']:
+			datastream_id = obj['Datastream']['@iot.id']
+			if datastream_id not in result_dict:
+				result_dict[datastream_id] = {
+					'Datastream': obj['Datastream'],
+					'components': obj['components'],
+					'dataArray': []
+				}
+			
+			result_dict[datastream_id]['dataArray'].extend(obj['dataArray'])
+
+	output_data = []
+
+	for key, value in result_dict.items():
+		output_data.append({
+			"Datastream": value["Datastream"],
+			"components": value["components"],
+			"dataArray": value["dataArray"]
+		})
+
+
+	url = "https://sensorthings.wecompair.eu/FROST-Server/v1.1/CreateObservations"
+	res = requests.post(url, json=output_data)
+	logger.debug(f"uploaded offline data to frost, return code {res}") 
+
